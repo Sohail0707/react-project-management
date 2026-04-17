a36ddf4b7a467dbf3c23962c61ae4a7ccf045727
sohail <sohail2001rana@gmail.com>
2026-04-17 15:16:37 +0530

    created a separate Button component and a window when no projects are selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
