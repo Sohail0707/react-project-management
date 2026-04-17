b2d3bd486f1f0864ac683850dc56dcb1a8e84a72
sohail <sohail2001rana@gmail.com>
2026-04-17 15:34:49 +0530

    implemented the Add Project button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleCreateProject() {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onProjectCreated={handleCreateProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onCreateProject={handleCreateProject} />;
+  }
+
   return (
     <main className="h-screen flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onCreateProject={handleCreateProject} />
+      {content}
     </main>
   );
 }
